f8688c25640ea982f55e579b51a0e183c43fe984
Elizabeth-grados <elizabethgradosl@gmail.com>
2025-08-30 12:51:09 -0500

    actualizacion 30 Agosto E v3

--- catalogo/views.py
+++ catalogo/views.py
@@ -946,11 +946,11 @@ def registrar_venta(request):
     nuevo_num = max_num + 1
     nro_documento = f"{tipo_cod_prefijo}-{nuevo_num:08d}"
 
-    metodo_pago_id = TblMetodoPago.objects.filter(metodo_pago_descrip='efectivo').values_list('id', flat=True).first()
+    metodo_pago_id = TblMetodoPago.objects.filter(metodo_pago_descrip='efectivo').values_list('metodo_pago_id', flat=True).first()
     if metodo_pago_id is None:
         raise ValueError("No se encontró metodo de pago registrado")
     
-    usuario_id = TblUsuario.objects.filter(username='venta-online').values_list('id', flat=True).first()
+    usuario_id = TblUsuario.objects.filter(username='venta-online').values_list('usuario_id', flat=True).first()
     if usuario_id is None:
         raise ValueError("No se encontró vendedor registrado")
 
